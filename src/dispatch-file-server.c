59a4f621b506819739fea3f3338c3c4d8a4812a7
William Cotton <williamcotton@gmail.com>
2022-01-04 09:05:12 -0600

    updates to file server

--- src/dispatch-file-server.c
+++ src/dispatch-file-server.c
@@ -6,11 +6,12 @@
 #include <regex.h>
 #include <dispatch/dispatch.h>
 
-void new_server(int *serverFd, int port)
+int listenServer(int port)
 {
+
   // Create the socket
-  *serverFd = -1;
-  if ((*serverFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  int servSock = -1;
+  if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
   {
     printf("socket() failed");
   }
@@ -23,174 +24,173 @@ void new_server(int *serverFd, int port)
   echoServAddr.sin_port = htons(port);
 
   int flag = 1;
-  if (-1 == setsockopt(*serverFd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)))
+  if (-1 == setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)))
   {
     perror("setsockopt fail");
   }
 
-  if (bind(*serverFd, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
+  if (bind(servSock, (struct sockaddr *)&echoServAddr, sizeof(echoServAddr)) < 0)
   {
     perror("Cannot bind socket");
     exit(2);
   }
 
-  if (listen(*serverFd, 1000) < 0)
+  // Make the socket non-blocking
+  if (fcntl(servSock, F_SETFL, O_NONBLOCK) < 0)
+  {
+    shutdown(servSock, SHUT_RDWR);
+    close(servSock);
+    perror("fcntl() failed");
+    exit(3);
+  }
+
+  if (listen(servSock, 1000) < 0)
   {
     perror("Listen error");
-    exit(3);
+    exit(4);
+  }
+
+  return servSock;
+}
+
+int acceptClient(int servSock)
+{
+  int clntSock = -1;
+  struct sockaddr_in echoClntAddr;
+  unsigned int clntLen = sizeof(echoClntAddr);
+  if ((clntSock = accept(servSock, (struct sockaddr *)&echoClntAddr, &clntLen)) < 0)
+  {
+    // perror("accept() failed");
+    return clntSock;
+  }
+
+  // Make the socket non-blocking
+  if (fcntl(clntSock, F_SETFL, O_NONBLOCK) < 0)
+  {
+    // perror("fcntl() failed");
+    shutdown(clntSock, SHUT_RDWR);
+    close(clntSock);
+  }
+  return clntSock;
+}
+
+FILE *matchFile(char *pattern, char buffer[1024])
+{
+  regex_t regex;
+  int reti;
+  size_t nmatch = 2;
+  regmatch_t pmatch[2];
+  reti = regcomp(&regex, pattern, REG_EXTENDED);
+  if (reti)
+  {
+    fprintf(stderr, "Could not compile regex\n");
+    exit(6);
+  }
+  reti = regexec(&regex, buffer, nmatch, pmatch, 0);
+  if (reti == 0)
+  {
+    // printf("Matched\n");
+    // printf("%lld %lld\n", pmatch[1].rm_so, pmatch[1].rm_eo);
+    char *path = buffer + pmatch[1].rm_so;
+    path[pmatch[1].rm_eo - pmatch[1].rm_so] = 0;
+    // printf("path: %s\n", path);
+    char *file = malloc(strlen(path) + 1);
+    strcpy(file, path);
+    // printf("file: %s\n", file);
+    char *file_path = malloc(strlen(file) + strlen("./files/") + 1);
+    strcpy(file_path, "./files/");
+    strcat(file_path, file);
+    // printf("file_path: %s\n", file_path);
+    FILE *fp = fopen(file_path, "r");
+    free(file_path);
+    free(file);
+    return fp;
+  }
+  else
+  {
+    return NULL;
+  }
+}
+
+void writeFile(FILE *fp, int clntSock)
+{
+  char buffer[1024];
+  while (1)
+  {
+    memset(buffer, 0, sizeof(buffer));
+    size_t size = fread(buffer, 1, sizeof(buffer), fp);
+    if (size < 0)
+    {
+      perror("read error");
+      exit(5);
+    }
+    if (size == 0)
+    {
+      break;
+    }
+    write(clntSock, buffer, size);
   }
 }
 
 int main()
 {
-  int serverFd;
+  int port = 1234;
+  int servSock = listenServer(port);
 
-  new_server(&serverFd, 1234);
+  dispatch_queue_t serverQueue = dispatch_queue_create("serverQueue", DISPATCH_QUEUE_CONCURRENT);
+  dispatch_source_t acceptSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, servSock, 0, serverQueue);
 
-  dispatch_semaphore_t exitsignal = dispatch_semaphore_create(0);
-
-  dispatch_queue_t dq = dispatch_queue_create("data", DISPATCH_QUEUE_CONCURRENT); // concurrent queue
-
-  dispatch_source_t ds = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, serverFd, 0, dq);
-
-  dispatch_source_set_event_handler(ds, ^{
-    printf("new source\n");
-
-    struct sockaddr_in client;
-    int clientFd;
-
-    socklen_t len = sizeof(client);
-
-    if ((clientFd = accept(serverFd, (struct sockaddr *)&client, &len)) < 0)
+  dispatch_source_set_event_handler(acceptSource, ^{
+    const unsigned long numPendingConnections = dispatch_source_get_data(acceptSource);
+    for (unsigned long i = 0; i < numPendingConnections; i++)
     {
-      perror("accept error");
-      exit(4);
-    }
+      int clntSock = acceptClient(servSock);
+      if (clntSock < 0)
+        continue;
 
-    // set setsockopt SO_NOSIGPIPE
-    int flag = 1;
-    if (-1 == setsockopt(clientFd, SOL_SOCKET, SO_NOSIGPIPE, &flag, sizeof(flag)))
-    {
-      perror("setsockopt fail");
-    }
-
-    char *client_ip = inet_ntoa(client.sin_addr);
-    printf("Accepted new connection from a client %s:%d\n", client_ip, ntohs(client.sin_port));
-
-    dispatch_queue_t dqc = dispatch_queue_create("client", DISPATCH_QUEUE_CONCURRENT); // concurrent queue
-
-    dispatch_async(dqc, ^{
-      char buffer[1024];
-      memset(buffer, 0, sizeof(buffer));
-      int size = read(clientFd, buffer, sizeof(buffer));
-      if (size < 0)
-      {
-        perror("read error");
-        exit(5);
-      }
-
-      printf("%s", buffer);
-
-      // a regular expression to match GET /:pathname HTTP/1.1
-      char *pattern = "GET /(.*) HTTP/1.[0-1]";
-      regex_t regex;
-      int reti;
-      size_t nmatch = 2;
-      regmatch_t pmatch[2];
-      reti = regcomp(&regex, pattern, REG_EXTENDED);
-      if (reti)
-      {
-        fprintf(stderr, "Could not compile regex\n");
-        exit(6);
-      }
-      reti = regexec(&regex, buffer, nmatch, pmatch, 0);
-      if (reti == 0)
-      {
-        printf("Matched\n");
-        printf("%lld %lld\n", pmatch[1].rm_so, pmatch[1].rm_eo);
-        char *path = buffer + pmatch[1].rm_so;
-        path[pmatch[1].rm_eo - pmatch[1].rm_so] = 0;
-        printf("path: %s\n", path);
-        char *file = malloc(strlen(path) + 1);
-        strcpy(file, path);
-        printf("file: %s\n", file);
-        char *file_path = malloc(strlen(file) + strlen("./") + 1);
-        strcpy(file_path, "./");
-        strcat(file_path, file);
-        printf("file_path: %s\n", file_path);
-        FILE *fp = fopen(file_path, "r");
-        if (fp == NULL)
+      dispatch_source_t readSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, clntSock, 0, serverQueue);
+      dispatch_source_set_event_handler(readSource, ^{
+        char request[1024];
+        ssize_t numBytes = read(clntSock, request, sizeof(request));
+        if (numBytes < 0)
         {
-          printf("File not found\n");
-          char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
-          write(clientFd, response, strlen(response));
-          close(clientFd);
+          // perror("read() failed");
           return;
         }
-        char *response = "HTTP/1.1 200 OK\r\n\r\n";
-        write(clientFd, response, strlen(response));
-        while (1)
+        else if (numBytes == 0)
         {
-          memset(buffer, 0, sizeof(buffer));
-          size = fread(buffer, 1, sizeof(buffer), fp);
-          if (size < 0)
-          {
-            perror("read error");
-            exit(5);
-          }
-          if (size == 0)
-          {
-            break;
-          }
-          write(clientFd, buffer, size);
+          return;
         }
-        free(file_path);
-        free(file);
-        fclose(fp);
-      }
-      else
-      {
-        printf("No match\n");
-        char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        write(clientFd, response, strlen(response));
-      }
 
-      close(clientFd);
+        printf("Request\n===\n%s\n", request);
 
-      regfree(&regex);
-    });
+        FILE *fp = matchFile("GET /files/(.*) HTTP/1.[0-1]", request);
+        if (fp != NULL)
+        {
+          char *response = "HTTP/1.1 200 OK\r\n\r\n";
+          printf("Response\n===\n%s", response);
+          write(clntSock, response, strlen(response));
+          writeFile(fp, clntSock);
+          fclose(fp);
+        }
+        else
+        {
+          char *response = "HTTP/1.1 404 Not Found\r\n\r\n";
+          printf("Response\n===\n%s", response);
+          write(clntSock, response, strlen(response));
+        }
+
+        shutdown(clntSock, SHUT_RDWR);
+        close(clntSock);
+      });
+      dispatch_resume(readSource);
+    }
   });
 
-  dispatch_source_set_cancel_handler(ds, ^{
-    printf("cancel\n");
-    int rfd = (int)dispatch_source_get_handle(ds);
-    close(rfd);
-    dispatch_semaphore_signal(exitsignal);
-  });
+  printf("Server listening on port %d\n\n", port);
+  dispatch_resume(acceptSource);
 
-  signal(SIGHUP, SIG_IGN);
-
-  dispatch_source_t sig_src = dispatch_source_create(DISPATCH_SOURCE_TYPE_SIGNAL, SIGHUP, 0, dispatch_get_main_queue());
-  dispatch_source_set_event_handler(sig_src, ^{
-    printf("Caught SIGHUP\n");
-    dispatch_semaphore_signal(exitsignal);
-  });
-  dispatch_resume(sig_src);
-
-  printf("waiting for clients\n");
-
-  dispatch_resume(ds);
-  dispatch_main(); // suspend run loop waiting for blocks
-
-  dispatch_semaphore_wait(exitsignal, dispatch_walltime(NULL, DISPATCH_TIME_FOREVER));
-
-  dispatch_release(ds);
-  dispatch_release(dq);
-  dispatch_release(exitsignal);
-
-  close(serverFd);
-
-  printf("server exit\n");
+  dispatch_main();
 
   return 0;
 }
