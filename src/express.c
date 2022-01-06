7b89fd96414554871ac574469da29f3b53e0ca9f
William Cotton <williamcotton@gmail.com>
2022-01-06 10:18:24 -0600

    fix some memory leaks, add HTTP response headers

--- src/express.c
+++ src/express.c
@@ -189,6 +189,7 @@ static request_t parseRequest(char *rawRequest, client_t client)
   req.method[method_len] = '\0';
   req.headers = headers;
   req.numHeaders = num_headers;
+  free(copy);
   return req;
 }
 
@@ -214,6 +215,9 @@ static void freeRequest(request_t req)
 {
   free(req.method);
   free(req.path);
+  free(req.url);
+  if (strlen(req.queryString) > 0)
+    free(req.queryString);
 }
 
 static response_t buildResponse()
@@ -223,15 +227,158 @@ static response_t buildResponse()
   return res;
 }
 
-// char *responseString = malloc(sizeof(char) * (strlen(data) + strlen(res.status) + strlen(res.contentType) + strlen(res.contentLength) + strlen(res.body) + 10));
-// sprintf(responseString, "HTTP/1.1 %s\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n%s", res.status, res.contentType, res.contentLength, res.body);
-// return responseString;
+static char *getStatusMessage(int status)
+{
+  switch (status)
+  {
+  case 100:
+    return "Continue";
+  case 101:
+    return "Switching Protocols";
+  case 102:
+    return "Processing";
+  case 103:
+    return "Early Hints";
+  case 200:
+    return "OK";
+  case 201:
+    return "Created";
+  case 202:
+    return "Accepted";
+  case 203:
+    return "Non-Authoritative Information";
+  case 204:
+    return "No Content";
+  case 205:
+    return "Reset Content";
+  case 206:
+    return "Partial Content";
+  case 207:
+    return "Multi-Status";
+  case 208:
+    return "Already Reported";
+  case 226:
+    return "IM Used";
+  case 300:
+    return "Multiple Choices";
+  case 301:
+    return "Moved Permanently";
+  case 302:
+    return "Found";
+  case 303:
+    return "See Other";
+  case 304:
+    return "Not Modified";
+  case 305:
+    return "Use Proxy";
+  case 306:
+    return "Switch Proxy";
+  case 307:
+    return "Temporary Redirect";
+  case 308:
+    return "Permanent Redirect";
+  case 400:
+    return "Bad Request";
+  case 401:
+    return "Unauthorized";
+  case 402:
+    return "Payment Required";
+  case 403:
+    return "Forbidden";
+  case 404:
+    return "Not Found";
+  case 405:
+    return "Method Not Allowed";
+  case 406:
+    return "Not Acceptable";
+  case 407:
+    return "Proxy Authentication Required";
+  case 408:
+    return "Request Timeout";
+  case 409:
+    return "Conflict";
+  case 410:
+    return "Gone";
+  case 411:
+    return "Length Required";
+  case 412:
+    return "Precondition Failed";
+  case 413:
+    return "Payload Too Large";
+  case 414:
+    return "URI Too Long";
+  case 415:
+    return "Unsupported Media Type";
+  case 416:
+    return "Range Not Satisfiable";
+  case 417:
+    return "Expectation Failed";
+  case 418:
+    return "I'm a teapot";
+  case 421:
+    return "Misdirected Request";
+  case 422:
+    return "Unprocessable Entity";
+  case 423:
+    return "Locked";
+  case 424:
+    return "Failed Dependency";
+  case 425:
+    return "Too Early";
+  case 426:
+    return "Upgrade Required";
+  case 428:
+    return "Precondition Required";
+  case 429:
+    return "Too Many Requests";
+  case 431:
+    return "Request Header Fields Too Large";
+  case 451:
+    return "Unavailable For Legal Reasons";
+  case 500:
+    return "Internal Server Error";
+  case 501:
+    return "Not Implemented";
+  case 502:
+    return "Bad Gateway";
+  case 503:
+    return "Service Unavailable";
+  case 504:
+    return "Gateway Timeout";
+  case 505:
+    return "HTTP Version Not Supported";
+  case 506:
+    return "Variant Also Negotiates";
+  case 507:
+    return "Insufficient Storage";
+  case 508:
+    return "Loop Detected";
+  case 510:
+    return "Not Extended";
+  case 511:
+    return "Network Authentication Required";
+  default:
+    return "Unknown";
+  }
+}
 
 static char *buildResponseString(char *data, response_t res)
 {
-  char *response = malloc(strlen("HTTP/1.1 200 OK\r\n\r\n") + strlen(data) + 1);
-  sprintf(response, "HTTP/1.1 %d OK\r\n\r\n%s", res.status, data);
-  return response;
+  char *contentType = "text/html; charset=utf-8";
+  char *contentLength = malloc(sizeof(char) * 20);
+  sprintf(contentLength, "%zu", strlen(data));
+  char *statusMessage = getStatusMessage(res.status);
+  char *status = malloc(sizeof(char) * (strlen(statusMessage) + 5));
+  sprintf(status, "%d %s", res.status, statusMessage);
+  char *headers = malloc(sizeof(char) * (strlen("HTTP/1.1 \r\nContent-Type: \r\nContent-Length: \r\n\r\n") + strlen(status) + strlen(contentType) + strlen(contentLength) + 1));
+  sprintf(headers, "HTTP/1.1 %s\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n", status, contentType, contentLength);
+  char *responseString = malloc(sizeof(char) * (strlen(headers) + strlen(data) + 1));
+  strcpy(responseString, headers);
+  strcat(responseString, data);
+  free(status);
+  free(headers);
+  free(contentLength);
+  return responseString;
 }
 
 static void initClientAcceptEventHandler()
@@ -327,6 +474,8 @@ int main()
 
     res->status = 201;
     res->send(response);
+
+    free(response);
   });
 
   app.listen(port, ^{
